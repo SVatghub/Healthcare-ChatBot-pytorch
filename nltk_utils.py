e6722bb32da4041e30fbe57c5943e3bae8ec1340
Sahil <diksha@mac.local>
2023-07-10 13:24:46 +0530

    adding more files

---
+++ nltk_utils.py
@@ -0,0 +1,31 @@
+import numpy as np
+import nltk
+# for first time only
+# nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(words),dtype=np.float32)
+    for word in words:
+        if(word in tokenized_sentence):
+            bag[words.index(word)]=1
+    return bag
